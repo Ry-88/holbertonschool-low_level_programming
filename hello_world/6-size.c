97361f5298d3436b94614e9b8ea1e1312a310a42
root <ryan-055@outlook.com>
2025-04-28 18:51:42 +0000

    done project :)

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu bytes\n", (unsigned long) sizeof(char));
+	printf("size of an int: %lu bytes\n", (unsigned long) sizeof(int));
+	printf("size of a long int: %lu bytes\n", (unsigned long) sizeof(long int));
+	printf("size of a long long int: %lu bytes\n", (unsigned long) sizeof(long long int));
+	printf("size of float: %lu bytes\n", (unsigned long) sizeof(float));
+	
+		return (0);
+}

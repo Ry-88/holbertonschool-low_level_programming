24cd347a8070bf32c1c17b140c9ccd3cbd23f880
root <ryan-055@outlook.com>
2025-04-28 17:07:17 +0000

    task 4 :)

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-	puts("Programming is like building a multilingual puzzle\n");
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }

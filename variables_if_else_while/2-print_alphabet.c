95321beb138f7cf788c596f98cefd05c901cae9a
root <ryan-055@outlook.com>
2025-04-30 17:22:31 +0000

    task 2 done :)

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	for (char C = 'a'; C <= 'z'; C++)
+	{
+		putchar(C);
+	}
+	putchar('\n');
+	return (0);
+}
--- variables_if_else_while/README.md
+++ variables_if_else_while/README.md
@@ -1,1 +1,5 @@
 variables, if, else and  while
+
+task 0: i use if , else if and else to check of the outbut positive or negative or 0
+
+task 1: i use if , else if and else to check of the last digit of outbut

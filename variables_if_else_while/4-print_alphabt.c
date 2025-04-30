d7d55e3f037ff3247c3c5fb08dda866feb4ac628
root <ryan-055@outlook.com>
2025-04-30 18:45:15 +0000

    task 4 done :)

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char C = 'a';
+
+	while (C <= 'z')
+	{
+		if (C != 'q' && C != 'e')
+		{
+			putchar(C);
+		}
+		
+		C++;
+	}
+
+	putchar('\n');
+	return (0);
+}

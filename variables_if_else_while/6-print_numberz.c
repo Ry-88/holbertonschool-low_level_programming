a66b43f3e0e6d2badf81f685069b43ffe6d2f6a5
root <ryan-055@outlook.com>
2025-05-01 14:57:24 +0000

    task 6 done :)

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int C = 060;
+
+	while (C <= 071)
+	{
+		putchar(C);
+		C++;
+	}
+
+	putchar('\n');
+	return (0);
+}

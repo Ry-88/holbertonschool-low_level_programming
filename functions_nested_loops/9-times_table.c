d3d994a7ef2f0f75d09659cff96a2c3884f201c6
root <ryan-055@outlook.com>
2025-05-05 22:06:00 +0000

    save

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,71 @@
+#include "main.h"
+/**
+ * times_table - check the code.
+ *
+ * Return: nothing
+ */
+
+void times_table(void)
+{
+	int n = 0;
+	int f = 0;
+	int c = 0;
+
+	while (n <= 9)
+	{
+		while (f <= 9)
+		{
+			if (n == 0)
+			{
+				_putchar(c + '0');
+				if (f != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+			} else if (n == 1)
+			{
+				_putchar(c + '0');
+				if (f != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				c++;
+			} else if (n == 2)
+			{
+				if (c > 9)
+				{
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+					if (f != 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+				else
+				{
+					_putchar(c + '0');
+					if (f != 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+				c += 2;
+			}
+
+			f++;
+		}
+		
+		c = 0;
+		n++;
+		f = 0;
+		_putchar('\n');
+	}
+
+}

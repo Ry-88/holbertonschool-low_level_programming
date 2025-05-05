f951a4f7718a99088d235a689fdbf275a91a6b3b
root <ryan-055@outlook.com>
2025-05-05 18:49:18 +0000

    task 6 done

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - check the code.
+ *
+ * @n: contain the number
+ *
+ * Return: positive of n
+ */
+
+int _abs(int n)
+{
+	int result = abs(n);
+
+	return (result);
+}

401dabafeb9380a58849d3864a1ff5c69b94d1c4
root <ryan-055@outlook.com>
2025-05-06 08:36:08 +0000

    task 10 done

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * add - function to add tow numbers
+ *
+ * @n: get first value.
+ *
+ * @s: get scound value.
+ *
+ * Return: total of n and s
+ */
+
+int add(int n, int s)
+{
+	int result;
+
+	result = n + s;
+
+	return (result);
+}

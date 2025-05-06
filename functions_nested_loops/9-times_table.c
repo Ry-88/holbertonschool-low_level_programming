a8420d13861ecafc88826c885b24352b536d6962
root <ryan-055@outlook.com>
2025-05-06 07:56:52 +0000

    task 9 done

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,63 +9,39 @@ void times_table(void)
 {
 	int n = 0;
 	int f = 0;
-	int c = 0;
+	int result = 0;
 
 	while (n <= 9)
 	{
 		while (f <= 9)
 		{
-			if (n == 0)
+			result = (f * n);
+			if (result == 0)
 			{
-				_putchar(c + '0');
-				if (f != 9)
+				if (f <= 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 				}
-			} else if (n == 1)
+				_putchar(result + '0');
+			} else if (result < 10)
 			{
-				_putchar(c + '0');
-				if (f != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				c++;
-			} else if (n == 2)
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			} else if (result >= 10)
 			{
-				if (c > 9)
-				{
-					if (f <= 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-					_putchar((c / 10) + '0');
-                                        _putchar((c % 10) + '0');
-				} else if (c == 0)
-				{
-					_putchar(c + '0');
-				}
-				else
-				{
-					if (f != 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar(c +'0');
-				}
-				c += 2;
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
 			}
 
 			f++;
 		}
 		
-		c = 0;
 		n++;
 		f = 0;
 		_putchar('\n');

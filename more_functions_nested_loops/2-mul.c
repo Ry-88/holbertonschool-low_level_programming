4faa5db75a5be86b25c736803618c0fb23e8e050
root <ryan-055@outlook.com>
2025-05-07 14:58:14 +0000

    task 2 done :)

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - check the code
+ *
+ * @a: contain first value
+ * @b: contain scound value
+ *
+ * Return: result of multiplies numbers
+ */
+
+int mul(int a, int b)
+{
+	int result = (a * b);
+
+	return (result);
+}

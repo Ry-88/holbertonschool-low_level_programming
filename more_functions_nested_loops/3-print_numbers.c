434c39c869b57d186ff0d9799ab3206d01608692
root <ryan-055@outlook.com>
2025-05-07 15:26:25 +0000

    task 3 done

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ *
+ */
+
+void print_numbers(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}

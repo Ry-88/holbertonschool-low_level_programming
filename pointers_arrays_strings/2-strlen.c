1c06bfe8f482aa69326580a918590b2ba4af6665
root <ryan-055@outlook.com>
2025-05-10 02:15:48 +0000

    task 2 done :)

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - count the number of latter
+ *
+ * @s: contain a word
+ *
+ * Return: n
+ */
+
+int _strlen(char *s)
+{
+	int n = strlen(s);
+
+	return (n);
+}

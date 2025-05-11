fb1e11b19834de47d39aea6b9ba0c896ad6a9353
root <ryan-055@outlook.com>
2025-05-11 07:28:54 +0000

    task 8 done

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print inegers
+ *
+ * @a: contain array
+ * @n: contain a number of array size
+ */
+
+void print_array(int *a, int n)
+{
+	int c = 0;
+
+	while (a[c] != '\0')
+	{
+		if (c == n - 1)
+		{
+			printf("%d", a[c]);
+			c++;
+		}
+		else
+		{
+			printf("%d, ", a[c]);
+			c++;
+		}
+	}
+
+	printf("\n");
+}

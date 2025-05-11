7c656b2cc6862fb50376c765e5ea9deb0a150ffa
root <ryan-055@outlook.com>
2025-05-11 18:18:17 +0000

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ *
+ * @dest: contain a copy of string
+ * @src: contain a string to copy for
+ * @n: size
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int b = 0;
+
+	while (b < n && src[b] != '\0')
+	{
+		dest[b] = src[b];
+		b++;
+	}
+
+	return (dest);
+}

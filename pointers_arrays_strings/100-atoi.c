ac2c34e8f782b4da6fa372b78efc6d74bead6ea8
root <ryan-055@outlook.com>
2025-05-11 09:57:21 +0000

    task 10 done :)

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _atoi - get the integer from string
+ *
+ * @s: contain a string to check
+ *
+ * Return: 0 if no number
+ */
+
+int _atoi(char *s)
+{
+	int n = 0, num = 0, found = 0, sign = 1;
+
+	while (s[n] != '\0')
+	{
+		if (s[n] == '-' && isdigit(s[n + 1]))
+		{
+			sign = -1;
+			n++;
+		}
+
+		if (isdigit(s[n]))
+		{
+			found = 1;
+			num = 0;
+
+			while (isdigit(s[n]))
+			{
+				num = num * 10 + (s[n] - '0');
+				n++;
+			}
+			break;
+		}
+		n++;
+	}
+
+	return found ? num * sign : 0;
+}

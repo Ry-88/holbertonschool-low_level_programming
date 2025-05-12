09a5f3c99596dd3b3e0ce58ad7949eca2120b13b
root <ryan-055@outlook.com>
2025-05-12 18:12:42 +0000

    task 2 done

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - check if character if found
+ *
+ * @s: contain a string to search on it
+ * @c: contain a latter to searched
+ *
+ * Return: c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+	{
+		if (s[n] == c)
+		{
+			s += n;
+			return (s);
+		}
+		n++;
+	}
+
+	return (NULL);
+}

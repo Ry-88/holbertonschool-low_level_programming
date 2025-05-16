436a8f6bf2e27cbfe88e4732c1b15e89432805bf
root <ryan-055@outlook.com>
2025-05-16 22:20:33 +0000

    task 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strstr - locates  a substring and change pointer
+ *
+ * @haystack: contain a string to search in it
+ * @needle: contain a string to search for it
+ *
+ * Return: pointer or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, count;
+
+	while (haystack[count])
+	{
+		count++;
+	}
+
+	for (i = 0; i < count; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			return (haystack + i);
+		}
+	}
+
+	return (NULL);
+}

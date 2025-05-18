7b5a19ff6e2622f9f09641dbee26ad71531ba347
root <ryan-055@outlook.com>
2025-05-18 14:06:44 +0000

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print the name file
+ *
+ * @argc: contain a size of argv
+ * @argv: contain a value of argment command line
+ *
+ * Return: allways 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

4d06334bb744358bd9dc74edcfa2f70ad7460e47
root <ryan-055@outlook.com>
2025-05-19 12:07:09 +0000

    task 0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - create an array and initializes it
+ *
+ * @size: contain a size of array
+ * @c: contain a char value
+ *
+ * Return: a pointer or 0 if fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int n = 0;
+	char *array;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+
+	array = malloc(size * sizeof(char));
+
+	while (n < size)
+	{
+		array[n] = c;
+		n++;
+	}
+
+	return (array);
+}

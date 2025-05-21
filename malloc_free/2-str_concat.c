fa2e36a9a4385bc1f32a8a38b0333460eef979bc
root <ryan-055@outlook.com>
2025-05-21 15:04:02 +0000

    task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenates tow string
+ *
+ * @s1: contain a first string
+ * @s2: contain a second string
+ *
+ * Return: pointer or NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *array;
+	int size1 = 0, size2 = 0, size3 = 0, num = 0, num2 = 0;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+
+	for (; s1[size1] != '\0'; size1++)
+	{
+	}
+
+	for (; s2[size2] != '\0'; size2++)
+	{
+	}
+
+	size1++, size2++;
+	size3 = size1 + size2;
+
+	array = malloc(size3 * sizeof(char));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (; num < size1 - 1; num++)
+	{
+		array[num] = s1[num];
+	}
+
+	while (num < size3 - 2)
+	{
+		array[num] = s2[num2];
+		num++, num2++;
+	}
+
+	return (array);
+}

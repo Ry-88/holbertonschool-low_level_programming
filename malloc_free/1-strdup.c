d3d960c8b1a24d754cdadac8c48700f54cbbeb01
root <ryan-055@outlook.com>
2025-05-21 13:43:47 +0000

    task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - return a pointer which duplicate by str
+ *
+ * @str: contain a string to duplicated
+ *
+ * Return: pointer or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+	int size = 0, num = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+
+	array = malloc(size * sizeof(char));
+
+	while (num < size)
+	{
+		array[num] = str[num];
+		num++;
+	}
+
+	return (array);
+}

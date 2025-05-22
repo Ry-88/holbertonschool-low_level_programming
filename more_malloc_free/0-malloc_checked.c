4e7df5a062f9efe60356450ecdc03fa72b41350f
root <ryan-055@outlook.com>
2025-05-22 15:59:30 +0000

    save

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: contain a size
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *s;
+
+	s = malloc(b * sizeof(b));
+}

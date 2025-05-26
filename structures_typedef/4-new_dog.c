4576b5b2b6b0a04c935a3a84ad69644cf45674fb
root <ryan-055@outlook.com>
2025-05-26 23:50:46 +0000

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,16 +10,39 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, j, num1 = 0, num2 = 0;
 	dog_t *dog2;
 
+	for (i = 0; name[i]; i++)
+	{
+	}
+	for (j = 0; owner[j]; j++)
+	{
+	}
 	dog2 = malloc(sizeof(dog_t));
 	if (dog2 == NULL)
 	{
 		return (NULL);
 	}
-	dog2->name = name;
+	dog2->name = malloc(i);
+	if (dog2->name == NULL)
+	{
+		return (NULL);
+	}
+	dog2->owner = malloc(j);
+	if (dog2->owner == NULL)
+	{
+		return (NULL);
+	}
+	for (; num1 < i; num1++)
+	{
+		dog2->name[num1] = name[num1];
+	}
+	for (; num2 < j; num2++)
+	{
+		dog2->owner[num2] = owner[num2];
+	}
 	dog2->age = age;
-	dog2->owner = owner;
 
 	return (dog2);
 }

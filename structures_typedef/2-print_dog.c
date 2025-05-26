ac70d47a1ff16990aaa024fc2b87a8d5e2b9d72a
root <ryan-055@outlook.com>
2025-05-26 18:30:36 +0000

    task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("Name: nil\n");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
@@ -23,7 +23,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->age < 0)
 	{
-		printf("Age: nil\n");
+		printf("Age: (nil)\n");
 	}
 	else
 	{
@@ -31,7 +31,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->owner == NULL)
 	{
-		printf("Owner: nil\n");
+		printf("Owner: (nil)\n");
 	}
 	else
 	{

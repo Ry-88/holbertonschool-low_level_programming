a6c2e5d35427fa4bf94179a162c9ef9395a91b35
root <ryan-055@outlook.com>
2025-05-27 01:34:22 +0000

    task 5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,9 +3,16 @@
 /**
  * free_dog - frees dogs
  * @d: contain addrecec of dog_t
+ * Return: if d is NULL
  */
 
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
 	free(d);
 }

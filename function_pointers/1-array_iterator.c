90b68a35f0f09880102cc94199e059c9192dbd92
root <ryan-055@outlook.com>
2025-05-28 15:30:53 +0000

    save

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator -  executes a function given as a parameter
+ * @array: array to contain an element
+ * @size: size of array
+ * @action: pointer to function call
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t n = 0;
+	array = malloc(size);
+	for (; n < size; n++)
+	{
+		action(*array);
+		array++;
+	}
+}

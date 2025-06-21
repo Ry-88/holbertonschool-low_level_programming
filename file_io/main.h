2bb8e534e8ef39a0d02f1af449d19e28c2aaacdc
root <ryan-055@outlook.com>
2025-06-21 22:13:10 +0000

    task 0 done

--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stddef.h>
 int _putchar(char c);
 size_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
